897b303c378b28e654a77175394c4a50c87d8c16
esthertrujillo <esther.trujillomartinez@usp.ceu.es>
2025-07-09 20:03:03 +0200

    so

--- utils/series_utils/scaling.py
+++
@@ -1,9 +0,0 @@
-from sklearn.preprocessing import StandardScaler
-import pandas as pd
-
-def escalar_datos(X_train, X_test):
-    scaler = StandardScaler()
-    X_train_scaled = pd.DataFrame(scaler.fit_transform(X_train), index=X_train.index, columns=X_train.columns)
-    X_test_scaled = pd.DataFrame(scaler.transform(X_test), index=X_test.index, columns=X_test.columns)
-    return X_train_scaled, X_test_scaled
-
